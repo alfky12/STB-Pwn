dbe7c9f7a06321570a1be89886aff689dbfb47ee
alfky12 <100685677+alfky12@users.noreply.github.com>
2024-06-09 10:31:04 +0900

    update updatestbpwn.sh

--- updatestbpwn.sh
+++ updatestbpwn.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 echo -e '\033[36mMenghentikan pipwn service\033[0m'
-sudo systemctl stop pipwn
 sleep 2
+sudo systemctl stop pipwn
 sudo find /boot/firmware/PPPwn ! -name 'config.sh' ! -name 'PPPwn' -exec rm -rf {} +
-echo -e '\033[36mUnduh skrip PI-Pwn terbaru\033[0m'
+echo -e '\033[36mMengunduh skrip PI-Pwn terbaru\033[0m'
+sleep 2
 cd PI-Pwn && git pull
 sudo cp -r PPPwn /boot/firmware && cd /boot/firmware/PPPwn
-echo -e '\033[36mMulai pembaruan\033[0m'
+echo -e '\033[36mMemulai pembaruan\033[0m'
+sleep 2
 sudo chmod 777 *
 sudo bash install.sh
