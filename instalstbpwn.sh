b799698308fb88cf1231b8ca27d86a8cf9ccbdbe
alfky12 <100685677+alfky12@users.noreply.github.com>
2024-06-09 10:29:37 +0900

    update instalstbpwn.sh

--- instalstbpwn.sh
+++ instalstbpwn.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+echo -e '\033[36mMemperbarui repo dan instal git\033[0m'
+sleep 2
 sudo apt update
 sudo apt install git -y
+echo -e '\033[36mMengunduh skrip STB-Pwn\033[0m'
+sleep 2
 git clone https://github.com/alfky12/STB-Pwn
 cd STB-Pwn
 sudo mkdir -p /boot/firmware
 sudo cp -r PPPwn /boot/firmware
 cd /boot/firmware/PPPwn
+echo -e '\033[36mMemulai instalasi\033[0m'
+sleep 2
 sudo chmod 777 *
 sudo bash install.sh
