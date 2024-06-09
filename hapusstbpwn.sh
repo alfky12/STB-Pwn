cf6b62f49187f70a4bd5136a11375ae6001c4b2e
alfky12 <100685677+alfky12@users.noreply.github.com>
2024-06-09 10:19:27 +0900

    update hapusstbpwn.sh

--- hapusstbpwn.sh
+++ hapusstbpwn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Hapus instalan PI-Pwn/STB-Pwn? (Y|N)"
+echo -e '\033[36mHapus instalan PI-Pwn/STB-Pwn? (Y|N)\033[0m'
 read -r delete_choice
 if [ "$delete_choice" == "Y" ] || [ "$delete_choice" == "y" ]; then
     sudo systemctl stop pipwn && sudo systemctl disable pipwn
@@ -8,23 +8,23 @@ if [ "$delete_choice" == "Y" ] || [ "$delete_choice" == "y" ]; then
     sudo rm -rf /boot/firmware/PPPwn
     sudo rm -rf PI-Pwn STB-Pwn
     sudo sed -i 's^sudo bash /boot/firmware/PPPwn/devboot.sh \&^^g' /etc/rc.local
-    echo "Hapus instalan berhasil. Muat ulang sekarang? (Y|N)"
+    echo -e '\033[36mHapus instalan berhasil. Muat ulang sekarang? (Y|N)\033[0m'
     read -r reboot_choice
     if [ "$reboot_choice" == "Y" ] || [ "$reboot_choice" == "y" ]; then
-        echo "Memulai muat ulang."
+        echo -e '\033[36mMemuat ulang.\033[0m'
         sudo reboot
     elif [ "$reboot_choice" == "N" ] || [ "$reboot_choice" == "n" ]; then
-        echo "Muat ulang dibatalkan."
+        echo -e '\033[36mMuat ulang dibatalkan.\033[0m'
         sudo systemctl daemon-reload
         exit 0
     else
-        echo "Input tidak valid. Keluar dari skrip."
+        echo -e '\033[36mInput tidak valid. Keluar dari skrip.\033[0m'
         exit 1
     fi
 elif [ "$delete_choice" == "N" ] || [ "$delete_choice" == "n" ]; then
-    echo "Batal hapus instalan."
+    echo -e '\033[36mBatal hapus instalan.\033[0m'
     exit 0
 else
-    echo "Input tidak valid. Keluar dari skrip."
+    echo -e '\033[36mInput tidak valid. Keluar dari skrip.\033[0m'
     exit 1
 fi
